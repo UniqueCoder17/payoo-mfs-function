23a1d57fc640eb010c2b99c5f153df761bf82951
UniqueCoder17 <raihanalamr08@gmail.com>
2024-09-21 23:31:56 +0600

    added transaction history

--- js/utilities.js
+++ js/utilities.js
@@ -20,3 +20,14 @@ function getTextFiledValueById(id){
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+
+
+function showSectionById(id){
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    // show the section with the provide id as parameter
+    document.getElementById(id).classList.remove('hidden');
+     
+}
